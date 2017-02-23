8507cd21cd19ee4bd8854ebcc840fa352ab3a6af
David Quisenberry and Jay Freeman <jay@>
2017-02-23 14:18:10 -0800

    create modified:   app/app.php new file:   views/add_cart.html.twig renamed:    views/delete_carts.html.twig -> views/cart_portal.html.twig new file:   views/cart_profile.html.twig deleted:    views/carts.html.twig new file:   views/create_account.html.twig deleted:    views/cuisine.html.twig deleted:    views/cuisine_edit.html.twig deleted:    views/cuisines.html.twig modified:   views/index.html.twig new file:   views/local_food_carts.html.twig new file:   views/owner_portal.html.twig

--- app/app.php
+++ app/app.php
@@ -3,6 +3,9 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Cart.php";
     require_once __DIR__."/../src/Cuisine.php";
+    require_once __DIR__."/../src/City.php";
+    require_once __DIR__."/../src/State.php";
+    require_once __DIR__."/../src/Address.php";
 
     $app = new Silex\Application();
 
@@ -21,55 +24,18 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get('/' , function() use ($app) {
-        return $app['twig']->render('index.html.twig' , array('cuisines' => Cuisine::getAll()));
+        return $app['twig']->render('index.html.twig');
     });
 
-    $app->get('/carts', function() use ($app) {
-        return $app['twig']->render('carts.html.twig' , array('carts' => Cart::getAll()));
+    $app->post('/local_food_carts' , function() use ($app) {
+        return $app['twig']->render('local_food_carts.html.twig');
     });
 
-    $app->post('/carts' ,function() use ($app) {
-        $cart = new Cart($_POST['name'], null, $_POST['cuisine_id'], $_POST['street'], $_POST['city'], $_POST['state'], intval($_POST['zip']), $_POST['phone'], $_POST['url']);
-        $cart->save();
-        return $app['twig']->render('index.html.twig' , array('cuisines' => Cuisine::getAll()));
+    $app->post('/login' , function() use ($app) {
+        return $app['twig']->render('index.html.twig');
     });
 
-    $app->get('/cuisines' , function() use ($app) {
-        return $app['twig']->render('cuisines.html.twig' , array('cuisines' => Cuisine::getAll()));
-    });
 
-    $app->post("/cuisines", function() use ($app) {
-        $cuisine = new Cuisine($_POST['name']);
-        $cuisine->save();
-        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
-    });
-
-    $app->post("/delete_cuisines", function() use ($app) {
-        Cuisine::deleteAll();
-        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
-    });
-
-    $app->post("/delete_carts", function() use ($app) {
-        Cart::deleteAll();
-        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
-    });
-
-    $app->get("/cuisines/{id}", function($id) use ($app) {
-        $cuisine = Cuisine::find($id);
-        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'carts' => $cuisine->getCarts()));
-    });
-
-    $app->get("/cuisines/{id}/edit", function($id) use ($app) {
-        $cuisine = Cuisine::find($id);
-        return $app['twig']->render('cuisine_edit.html.twig', array('cuisine' => $cuisine));
-    });
-
-    $app->patch("/cuisines/{id}", function($id) use ($app) {
-        $name = $_POST['name'];
-        $cuisine = Cuisine::find($id);
-        $cuisine->update($name);
-        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'carts' => $cuisine->getCarts()));
-    });
 
     return $app;
 ?>
